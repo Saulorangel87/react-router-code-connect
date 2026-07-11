58ab743a9281d0e00ee739d311199f2c796670c6
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-11 16:07:05 -0300

    feat: initialize application router and main entry point with route configuration

---
+++ src/Router/index.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Register } from "../pages/Register/index.jsx";
+import { Login } from "../pages/Login/index.jsx";
+import { Logout } from "../pages/Logout/index.jsx";
+import { ProtectedRoute } from "../components/ProtectedRoute/index.jsx";
+import { Feed } from "../pages/Feed/index.jsx";
+import { BlogPost } from "../pages/BlogPost/index.jsx";
+
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth">
+                    <Route path="register" element={<Register />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="logout" element={<Logout />} />
+                </Route>
+                <Route path="/">
+                    <Route
+                        path=""
+                        element={
+                            <ProtectedRoute>
+                                <Feed />
+                            </ProtectedRoute>
+                        }
+                    />
+
+                    <Route
+                        path="blog-post/:slug"
+                        element={
+                            <ProtectedRoute>
+                                <BlogPost />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
