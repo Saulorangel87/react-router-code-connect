520911325438341636d3d89a93dea9cffd03a383
Saulo Rangel <saulorangel10@hotmail.com>
2026-07-11 16:52:14 -0300

    feat: implement application routing, authentication layouts, and core page views

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -5,18 +5,21 @@ import { Logout } from "../pages/Logout/index.jsx";
 import { ProtectedRoute } from "../components/ProtectedRoute/index.jsx";
 import { Feed } from "../pages/Feed/index.jsx";
 import { BlogPost } from "../pages/BlogPost/index.jsx";
+import { AuthLayout } from "../layouts/Auth/index.jsx";
+import { AppLayout } from "../layouts/App/index.jsx";
+import { NotFound } from "../pages/NotFound/index.jsx";
 
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/auth">
+                <Route path="/auth" element={<AuthLayout />}>
                     <Route path="register" element={<Register />} />
                     <Route path="login" element={<Login />} />
                     <Route path="logout" element={<Logout />} />
                 </Route>
-                <Route path="/">
+                <Route path="/" element={<AppLayout />}>
                     <Route
                         path=""
                         element={
@@ -34,6 +37,7 @@ export const AppRouter = () => {
                             </ProtectedRoute>
                         }
                     />
+                    <Route path='*' element={<NotFound />} />
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,9 @@
+export const NotFound = () => {
+    return (
+        <main>
+            <h1 style={{color: "white"}}>
+            Error 404, página não encontrada!
+            </h1>
+        </main>
+    )
+}
